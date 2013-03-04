c324a86ce22eac73f34083f3e3e7065ce424ca5e
Robert C. Uhl <rcuhl@sep.com>
2013-03-04 11:10:20 -0500

    Swapping to GPL v2.

--- main.rb
+++ main.rb
@@ -1,6 +1,18 @@
-# A pathfinder character creation utility
 # Author:: Robert Uhl (mailto:uhlrc@yahoo.com)
-# License:: MIT License
+# License:: GPL V2
+# Copyright:: (C) 2013  Robert Uhl
+
+#--
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#++
 
 require 'gtk2'
 require 'logger'
