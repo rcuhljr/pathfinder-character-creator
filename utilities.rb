8af078d1674e56c2e84159267c310d284f596692
Robert C. Uhl <rcuhl@sep.com>
2013-04-05 13:49:55 -0400

    False alarm, staying in gtk2. Made ListStores marshalable.

--- utilities.rb
+++ utilities.rb
@@ -20,22 +20,32 @@ class Fixnum
   end
 end
 
-module Gtk
-  
+module Gtk  
   class ListStore
-    def marshal_dump
+    def _dump level
       arr_of_arr = []
-      self.each do |item| 
+      headers = []
+      arr_of_arr << headers
+      self.each do |model, path, iter| 
         arr = []
         count = 0
-        while not item[count].nil? 
-          arr << item[count]        
+        while not iter[count].nil? 
+          headers << iter[count].class if arr_of_arr.length == 1
+          arr << iter[count]        
           count += 1
         end
         arr_of_arr << arr
       end
-      return arr_of_arr.marshal_dump
+      return Marshal.dump(arr_of_arr)
+    end  
+    
+    def self._load(data)
+      data = Marshal.load(data)
+      headers = data.shift      
+      store = new(*headers)      
+      data.each{|item_row| item_row.each_with_index{ |value, index| 
+         iter = store.append(); iter[index] = value }}
+      return store
     end
-  
   end
 end
